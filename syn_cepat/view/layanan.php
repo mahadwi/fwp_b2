635ea0eed20f93721a99ee2756fb4a099706665e
mahadwiputra008 <mahadwipura008@gmail.com>
2020-04-18 13:46:26 +0700

    menambah folder syn_cepat

---
+++ syn_cepat/view/layanan.php
@@ -0,0 +1,64 @@
+<!-- ITEM -->
+<section class="layanan-item">
+  <h1>Layanan Yang Menyesuaikan Segala <br>
+    Kebutuhan Anda</h1>
+  <h6>Divisi ekspress JNE melayani kiriman paket dan dokumen, waktu tujuan dalam negeri <br>
+    melalui lebih 1.500 titik layanan ekslusif dari penjemputan hingga pengantaran yang tersebar <br>
+    di seluruh Indonesia</h6>
+  <div class="row justify-content-center">
+    <div class="col-4">
+      <div class="item-yes">
+        <img src="assets/icon/003-airplane-1.png" alt="yes"> <br>
+        <h2><a href="?page=yes">YES</a></h2>
+        <h6>Yes adalah layanan dengan waktu <br>
+          penyampaian di tujuan keesokan hari <br>
+          (termasuk Minggu dan hari libur Nasional)</h6>
+      </div>
+    </div>
+    <div class="col-4">
+      <div class="item-yes">
+        <img src="assets/icon/004-motorcycle.png" alt="reg"> <br>
+        <h2><a href="?page=reg">REG</a></h2>
+        <h6>Reguler adalah layanan pengiriman ke <br>
+          seluruh wilayah Indonesia, dengan perkiraan
+          waktu penyampaian kiriman 1-7 hari kerja</h6>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Akhir ITEM -->
+
+<!-- RATING -->
+<section class="rating">
+  <div class="container">
+    <h2>Apa Kata Mereka <br> Tentang Kami ?</h2>
+    <hr style="margin-bottom: 70px;">
+    <div class="row">
+      <div class="col-6">
+        <h6>kami sudah menggunakan jasa syn cepat sejak <br>
+          pertama kami memulai bisnis kami 2019, hingga saat <br>
+          ini kami bisa lebih luas lagi menjangkau ke daerah <br>
+          seluruh wilayah Indonesia</h6>
+        <hr>
+        <h5 style="font-family: lora; font-size: 20px; font-weight: bold; color: #302683;">Maha Dwi Putra</h5>
+        <img src="assets/icon/Rate1.png" alt="rate"> <br>
+        <button type="submit" class="tombol-rating">Berikan Rating</button>
+      </div>
+      <div class="col-3">
+        <div class="rate-1">
+          <h1>4.8</h1>
+          <h3>Mariyah</h3>
+          <img src="assets/icon/Rate2.png" alt="Rate" width="166px" height="30.11px" style="margin-top: 26px;">
+        </div>
+      </div>
+      <div class="col-3">
+        <div class="rate-1">
+          <h1>4.9</h1>
+          <h3>MDP</h3>
+          <img src="assets/icon/Rate2.png" alt="Rate" width="166px" height="30.11px" style="margin-top: 26px;">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- AKHIR RATING -->
---
+++ syn_cepat/view/reg.php
@@ -0,0 +1,35 @@
+<!-- REG -->
+<section class="yes justify-content-center">
+  <div class="container">
+    <div class="row text-center">
+      <div class="col">
+        <div class="judul-yes">
+          <h1>REG (Regular)</h1>
+          <hr style="background-color: white; width:114px; border-width: 3px; margin-bottom: 70px;">
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="container">
+        <div class="yes-content">
+          <div class="col-6 mx-auto d-block text-center">
+            <h1>Hitung estimasi biaya pengiriman paket anda <br>
+              dengan layanan REG</h1><br>
+            <p>
+              <input type=" text" class="form-control" placeholder="Kota Asal">
+            </p>
+            <p>
+              <input type="text" class="form-control" placeholder="Kota Tujuan">
+            </p>
+            <p>
+              <input type="text" class="form-control" placeholder="Berat Barang (grams)">
+            </p>
+            <button type="button" class="btn btn-outline-primary">Clear</button>
+            <button type="button" class="btn btn-outline-success">Cek Biaya</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- AKHIR REG -->
